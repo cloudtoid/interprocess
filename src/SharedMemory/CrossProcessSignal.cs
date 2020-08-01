1375878aeafe12808ec784464699b82bc836f8e7
prezaei <pedram@rezaei.us>
2020-08-01 14:37:35 -0700

    Better cross process signaling for Unix based systems

--- src/SharedMemory/CrossProcessSignal.cs
+++ src/SharedMemory/CrossProcessSignal.cs
@@ -20,38 +20,61 @@ namespace Cloudtoid.SharedMemory
 
         private sealed class CrossProcessWindowsSignal : CrossProcessSignal
         {
+            private const string HandleNamePrefix = "SMQ_";
             private readonly EventWaitHandle handle;
 
             internal CrossProcessWindowsSignal(string queueName)
             {
-                handle = new EventWaitHandle(true, EventResetMode.AutoReset, "SMQ_" + queueName);
+                handle = new EventWaitHandle(true, EventResetMode.AutoReset, HandleNamePrefix + queueName);
             }
 
-            public override void Dispose() => handle.Dispose();
-            internal override void Signal() => handle.Set();
-            internal override void Wait(int millisecondsTimeout) => handle.WaitOne(millisecondsTimeout);
+            public override void Dispose()
+                => handle.Dispose();
+
+            internal override void Signal()
+                => handle.Set();
+
+            internal override void Wait(int millisecondsTimeout)
+                => handle.WaitOne(millisecondsTimeout);
         }
 
         private sealed class CrossProcessUnixSignal : CrossProcessSignal
         {
+            private const string FileExtension = ".fw";
             private readonly string filePath;
+            private readonly AutoResetEvent handle;
             private readonly FileSystemWatcher watcher;
 
             internal CrossProcessUnixSignal(string queueName, string path)
             {
-                var fileName = queueName + ".fw";
+                var fileName = queueName + FileExtension;
                 filePath = Path.Combine(path, fileName);
 
                 if (!File.Exists(filePath))
-                    File.WriteAllText(filePath, "sync file");
+                    File.WriteAllText(filePath, "shared memory queue sync file");
+
+                handle = new AutoResetEvent(true);
 
                 watcher = new FileSystemWatcher(path, fileName);
+                watcher.NotifyFilter = NotifyFilters.LastAccess;
+                watcher.Changed += OnChanged;
+                watcher.EnableRaisingEvents = true;
             }
 
-            public override void Dispose() => watcher.Dispose();
-            internal override void Signal() => File.SetLastAccessTimeUtc(filePath, DateTime.UtcNow);
+            public override void Dispose()
+            {
+                watcher.Dispose();
+                handle.Dispose();
+            }
+
+            internal override void Signal()
+                => File.SetLastAccessTimeUtc(filePath, DateTime.UtcNow);
+
             internal override void Wait(int millisecondsTimeout)
-                => watcher.WaitForChanged(WatcherChangeTypes.Changed, millisecondsTimeout);
+                => handle.WaitOne(millisecondsTimeout);
+
+            private void OnChanged(object _, FileSystemEventArgs e)
+                => handle.Set();
         }
     }
 }   
