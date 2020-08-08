2b74ad0591e4b15822c8d056140b208f89a8e579
prezaei <pedram@rezaei.us>
2020-08-08 15:14:50 -0700

    minor refactoring

--- src/Interprocess/Semaphore/IInterprocessSemaphore.cs
+++ src/Interprocess/Semaphore/IInterprocessSemaphore.cs
@@ -4,6 +4,11 @@ using System.Threading.Tasks;
 
 namespace Cloudtoid.Interprocess
 {
+    internal interface IInterprocessSemaphoreWaiter : IDisposable
+    {
+        bool WaitOne(int millisecondsTimeout);
+    }
+
     internal interface IInterprocessSemaphoreReleaser : IDisposable
     {
         Task ReleaseAsync(CancellationToken cancellation);
--- src/Interprocess/Semaphore/IInterprocessSemaphoreWaiter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Cloudtoid.Interprocess
-{
-    internal interface IInterprocessSemaphoreWaiter : IDisposable
-    {
-        bool WaitOne(int millisecondsTimeout);
-    }
-}
