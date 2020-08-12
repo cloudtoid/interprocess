91933c021b109d92ccfc6f8192deb794080c6424
prezaei <pedram@rezaei.us>
2020-08-11 22:16:17 -0700

    support for multiple runtimes

--- src/Interprocess/Properties/AssemblyInfo.cs
+++ src/Interprocess/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Interprocess.Tests")]
+[assembly: InternalsVisibleTo("Cloudtoid.Interprocess.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // needed by NSubstitute
