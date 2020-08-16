6a0d51ef42c34628d0c8e1461d9f12ebc3a51a99
prezaei <pedram@rezaei.us>
2020-08-16 01:31:54 -0700

    removing unused using statements

--- src/Interprocess.Tests/SemaphoreUnixTests.cs
+++ src/Interprocess.Tests/SemaphoreUnixTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Cloudtoid.Interprocess.Semaphore.Unix;
 using FluentAssertions;
