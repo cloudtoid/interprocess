c7f7e2b859850c14fd4defa2b75fc698cdd0682c
prezaei <pedram@rezaei.us>
2020-08-25 10:12:32 -0700

    one more

--- src/Interprocess/DependencyInjection.cs
+++ src/Interprocess/DependencyInjection.cs
@@ -5,6 +5,11 @@ namespace Cloudtoid.Interprocess
 {
     public static class DependencyInjection
     {
+        /// <summary>
+        /// Registers what is needed to create and consume shared-memory queues that are
+        /// cross-process accessible.
+        /// Use <see cref="IQueueFactory"/> to access the queue.
+        /// </summary>
         public static IServiceCollection AddInterprocessQueue(this IServiceCollection services)
         {
             CheckValue(services, nameof(services));
