61dd02aa6e5b73b1d64dda7ed2ee2f2b1b0bf99a
prezaei <pedram@rezaei.us>
2020-09-16 17:28:18 -0700

    set of fixes

--- src/Interprocess/Semaphore/SemaphoreLinux.cs
+++ src/Interprocess/Semaphore/SemaphoreLinux.cs
@@ -49,7 +49,7 @@ namespace Cloudtoid.Interprocess
         private static class Interop
         {
             private const string Lib = "librt";
-            private const int O_CREATE = 00000100; // create the semaphore if it does not exist
+            private const int O_CREAT = 0x00000040; // create the semaphore if it does not exist
             private const int SEM_VALUE_MAX = 32767;
 
             /// <summary>
@@ -89,11 +89,6 @@ namespace Cloudtoid.Interprocess
             private const int ENFILE = 23;
 
             /// <summary>
-            /// The semaphore is already locked.
-            /// </summary>
-            private const int EAGAIN = 11;
-
-            /// <summary>
             ///  O_CREAT is not set and the named semaphore does not exist.
             /// </summary>
             private const int ENOENT = 2;
@@ -108,10 +103,39 @@ namespace Cloudtoid.Interprocess
             /// </summary>
             private const int EOVERFLOW = 75;
 
+            /// <summary>
+            /// The call timed out before the semaphore could be locked.
+            /// </summary>
+            private const int ETIMEDOUT = 110;
+
+            [Flags]
+            private enum FilePermissions : uint
+            {
+                S_ISUID = 0x0800, // Set user ID on execution
+                S_ISGID = 0x0400, // Set group ID on execution
+                S_ISVTX = 0x0200, // Save swapped text after use (sticky).
+                S_IRUSR = 0x0100, // Read by owner
+                S_IWUSR = 0x0080, // Write by owner
+                S_IXUSR = 0x0040, // Execute by owner
+                S_IRGRP = 0x0020, // Read by group
+                S_IWGRP = 0x0010, // Write by group
+                S_IXGRP = 0x0008, // Execute by group
+                S_IROTH = 0x0004, // Read by other
+                S_IWOTH = 0x0002, // Write by other
+                S_IXOTH = 0x0001, // Execute by other
+
+                S_IRWXG = S_IRGRP | S_IWGRP | S_IXGRP,
+                S_IRWXU = S_IRUSR | S_IWUSR | S_IXUSR,
+                S_IRWXO = S_IROTH | S_IWOTH | S_IXOTH,
+                ACCESSPERMS = S_IRWXU | S_IRWXG | S_IRWXO, // 0777
+                ALLPERMS = S_ISUID | S_ISGID | S_ISVTX | S_IRWXU | S_IRWXG | S_IRWXO, // 07777
+                DEFFILEMODE = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH, // 0666
+            }
+
             private static unsafe int errno => Marshal.GetLastWin32Error();
 
             [DllImport(Lib, SetLastError = true)]
-            private static extern IntPtr sem_open(string name, int mode, ushort permission, uint initialCount);
+            private static extern IntPtr sem_open([MarshalAs(UnmanagedType.LPUTF8Str)] string name, int oflag, uint mode, uint value);
 
             [DllImport(Lib, SetLastError = true)]
             private static extern int sem_post(IntPtr handle);
@@ -120,13 +144,10 @@ namespace Cloudtoid.Interprocess
             private static extern int sem_wait(IntPtr handle);
 
             [DllImport(Lib, SetLastError = true)]
-            private static extern int sem_trywait(IntPtr handle);
-
-            [DllImport(Lib, SetLastError = true)]
             private static extern int sem_timedwait(IntPtr handle, ref Timespec abs_timeout);
 
             [DllImport(Lib, SetLastError = true)]
-            private static extern int sem_unlink(string name);
+            private static extern int sem_unlink([MarshalAs(UnmanagedType.LPUTF8Str)] string name);
 
             [DllImport(Lib, SetLastError = true)]
             private static extern int sem_close(IntPtr handle);
@@ -134,7 +155,7 @@ namespace Cloudtoid.Interprocess
             internal static IntPtr CreateOrOpenSemaphore(string name, uint initialCount)
             {
                 // 777 == Read, Write, and Execute permissions for Owner, Group, and Others
-                var handle = sem_open(name, O_CREATE, 666, initialCount);
+                var handle = sem_open(name, O_CREAT, (uint)FilePermissions.ACCESSPERMS, initialCount);
                 if (handle != IntPtr.Zero)
                     return handle;
 
@@ -144,7 +165,7 @@ namespace Cloudtoid.Interprocess
                     ENAMETOOLONG => new ArgumentException($"The specified semaphore name is too long.", nameof(name)),
                     EACCES => new SempahoreUnauthorizedAccessException(),
                     EEXIST => new SempahoreExistsException(),
-                    EINTR => new SempahoreException("The sem_open() operation was interrupted by a signal."),
+                    EINTR => new OperationCanceledException(),
                     ENFILE => new SempahoreException("Too many semaphores or file descriptors are open on the system."),
                     EMFILE => new SempahoreException("Too many semaphores or file descriptors are open by the process."),
                     ENOMEM => new InsufficientMemoryException(),
@@ -178,39 +199,30 @@ namespace Cloudtoid.Interprocess
                 };
             }
 
-            internal static bool TryWait(IntPtr handle)
-            {
-                if (sem_trywait(handle) == 0)
-                    return true;
-
-                return errno switch
-                {
-                    EAGAIN => false,
-                    EINVAL => throw new InvalidSempahoreException(),
-                    EINTR => throw new OperationCanceledException(),
-                    _ => throw new SempahoreException(errno),
-                };
-            }
-
             internal static bool Wait(IntPtr handle, int millisecondsTimeout)
             {
                 if (millisecondsTimeout == Timeout.Infinite)
                 {
                     Wait(handle);
+                    return true;
                 }
-                else
+
+                var timespec = new Timespec
                 {
-                    var start = DateTime.Now;
-                    while (!TryWait(handle))
-                    {
-                        if ((DateTime.Now - start).Milliseconds > millisecondsTimeout)
-                            return false;
+                    tv_sec = millisecondsTimeout / 1000,
+                    tv_nsec = 1000_000 * (millisecondsTimeout % 1000),
+                };
 
-                        Thread.Yield();
-                    }
-                }
+                if (sem_timedwait(handle, ref timespec) == 0)
+                    return true;
 
-                return true;
+                return errno switch
+                {
+                    ETIMEDOUT => false,
+                    EINVAL => throw new InvalidSempahoreException(),
+                    EINTR => throw new OperationCanceledException(),
+                    _ => throw new SempahoreException(errno),
+                };
             }
 
             internal static void Close(IntPtr handle)
@@ -288,7 +300,7 @@ namespace Cloudtoid.Interprocess
     internal class SempahoreUnauthorizedAccessException : SempahoreException
     {
         public SempahoreUnauthorizedAccessException()
-            : base("The required permissions (for reading and/or writing) are denied for the given flags; or O_CREAT is specified, the object does not exist, and permission to create the semaphore is denied.")
+            : base("The semaphore exists, but the caller does not have permission to open it.")
         {
         }
     }
